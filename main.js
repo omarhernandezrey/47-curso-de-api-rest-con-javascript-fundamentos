9483cce30ed7830c6734833c3c4a86a221df2175
omarhernandezrey <hernandezreyomar@gmail.com>
2024-11-30 17:10:00 -0500

    GET: leyendo michis favoritos

--- main.js
+++ main.js
@@ -1,27 +1,43 @@
-console.log("hello world");
-
 // URL de la API con un límite de 3 imágenes y una API Key válida
-const API_URL = "https://api.thedogapi.com/v1/images/search?limit=3&api_key=live_Uy3Pqxp9u5fLwZM7KhvVlaV7GGvyul8QKQze9fU4Q1qym2AD4b1wonrzK17h1Nuk";
+const API_URL_RAMDOM = "https://api.thedogapi.com/v1/images/search?limit=2&api_key=live_Uy3Pqxp9u5fLwZM7KhvVlaV7GGvyul8QKQze9fU4Q1qym2AD4b1wonrzK17h1Nuk";
+const API_URL_FAVORITES = "https://api.thedogapi.com/v1/favourites?limit=2&api_key=live_Uy3Pqxp9u5fLwZM7KhvVlaV7GGvyul8QKQze9fU4Q1qym2AD4b1wonrzK17h1Nuk";
 
-// Función asíncrona para recargar las imágenes de perros
-async function reload() {
-  // Realizamos una solicitud a la API y esperamos la respuesta
-  const res = await fetch(API_URL);
-  // Convertimos la respuesta en un objeto JSON
+const spanError = document.getElementById("error")
+
+
+async function loadRamdomDogs() {
+  const res = await fetch(API_URL_RAMDOM);
   const data = await res.json();
+  console.log('Random'); 
+  console.log(data); 
 
-  console.log(data); // Mostramos los datos obtenidos en consola para depuración
 
-  // Seleccionamos los elementos <img> del documento
-  const img1 = document.getElementById("dogImg1");
-  const img2 = document.getElementById("dogImg2");
-  const img3 = document.getElementById("dogImg3");
-
-  // Asignamos las URLs de las imágenes obtenidas a los elementos <img>
-  img1.src = data[0].url;
-  img2.src = data[1].url;
-  img3.src = data[2].url;
+  if(res.status !==200) {
+    spanError.innerHTML = "Hubo un error: " + res.status;
+  }else {
+    const img1 = document.getElementById("dogImg1");
+    const img2 = document.getElementById("dogImg2");
+  
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+  }
 }
 
-// Llamamos a la función para cargar las imágenes al inicio
-reload();
+
+async function loadFavoritesDogs() {
+  
+    const res = await fetch(API_URL_FAVORITES);
+
+    const data = await res.json();
+    console.log('Favoritos');
+    
+    console.log(data); 
+    if(res.status !==200) {
+        spanError.innerHTML = "Hubo un error: " + res.status + data.message ;
+      }
+  
+  }
+
+
+loadRamdomDogs();
+loadFavoritesDogs();
