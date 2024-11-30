336e62cd7dcbc2a20992965ac654a67d98810fa2
omarhernandezrey <hernandezreyomar@gmail.com>
2024-11-30 15:14:21 -0500

    Endpoints y query parameters

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
-// Imprimimos un mensaje en la consola para confirmar que el programa está corriendo correctamente.
 console.log("hello world");
 
-// Declaramos una constante llamada 'URL' que contiene la dirección de la API.
-// Esta API devuelve imágenes aleatorias de perros.
-const URL = "https://api.thedogapi.com/v1/images/search";
+const API_URL = "https://api.thedogapi.com/v1/images/search?limit=3";
 
-// Llamamos a la función 'fetch' para realizar una solicitud HTTP a la API.
-// 'fetch' devuelve una promesa que se resolverá cuando la respuesta esté disponible.
-fetch(URL)
-  // Cuando la solicitud se resuelve, obtenemos una respuesta (objeto 'Response').
-  .then(res => res.json()) // Convertimos el cuerpo de la respuesta en un objeto JavaScript usando 'json()'.
-  
-  // Cuando el JSON se ha convertido, pasamos los datos al siguiente bloque '.then'.
-  .then(data => {
-    // Seleccionamos la primera etiqueta <img> del documento usando 'querySelector'.
-    const img = document.querySelector("img");
+  async function reload() {
+    const res = await fetch(API_URL)
+    const data = await res.json();
 
-    // Asignamos la URL de la imagen recibida desde la API al atributo 'src' de la etiqueta <img>.
-    // 'data[0].url' contiene la URL de la imagen que queremos mostrar.
-    img.src = data[0].url;
-  });
+    console.log(data)
+    const img1 = document.getElementById("img1");
+    const img2 = document.getElementById("img2");
+    const img3 = document.getElementById("img3");
+    
+    
+    img1.src =data[0].url;
+    img2.src =data[1].url;
+    img3.src =data[2].url;
+  }
+reload();
